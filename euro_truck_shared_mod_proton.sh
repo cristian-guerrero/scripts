616e785c3d2dc994a57a09ac3903cf7f5203e525
Cristian Guerrero <guerrero.chavez.cristian@gmail.com>
2021-07-11 11:40:18 -0500

    euro truck mod

---
+++ euro_truck_shared_mod_proton.sh
@@ -0,0 +1,1 @@
+ln -s "/home/cristian/.local/share/Euro Truck Simulator 2/mod" "/home/cristian/.local/share/Steam/steamapps/compatdata/227300/pfx/drive_c/users/steamuser/Documents/Euro Truck Simulator 2/mod"
